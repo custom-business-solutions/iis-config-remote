da936f542372ffb6437999e8b0e5d83673743671
Bernardo Pacheco <berna.ipach@gmail.com>
2016-04-25 10:00:17 -0400

    Made it a menubar app

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
 var registerIpcListeners = require('./lib/ipcUtils').registerIpcListeners;
+var mb = require('menubar')({dir: 'public'});
 
 require('crash-reporter').start();
 
-process.noDeprecation = true;
+// app.on('window-all-closed', function() {
+//     if (process.platform != 'darwin') {
+//         app.quit();
+//     }
+// });
 
-app.on('window-all-closed', function() {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
-});
+// app.on('ready', function() {
+//     mainWindow = new BrowserWindow({width: 1280, height: 800});
+//     mainWindow.loadUrl('file://' + __dirname + '/public/index.html');
+//     mainWindow.openDevTools();
+//
+//     registerIpcListeners();
+//
+//     mainWindow.on('closed', function() {
+//         mainWindow = null;
+//     });
+// });
 
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({width: 1280, height: 800});
-    mainWindow.loadUrl('file://' + __dirname + '/public/index.html');
-    mainWindow.openDevTools();
-
+mb.on('ready', function () {
     registerIpcListeners();
-
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
 });
