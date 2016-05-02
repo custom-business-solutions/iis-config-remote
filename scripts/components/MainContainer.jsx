abebbd74303970e3b1d9fb61b046771301c6ae5e
Bernardo Pacheco <berna.ipach@gmail.com>
2016-05-02 10:16:48 -0400

    Cleaned up npm scripts and started process of re-writing the app

---
+++ scripts/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+// var ipcRenderer = window.require('electron').ipcRenderer
+
+import React, { Component } from 'react'
+
+import '../../sass/main.scss'
+
+export default class MainContainer extends Component {
+  render () {
+    return <div></div>
+  }
+}
