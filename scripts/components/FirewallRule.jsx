275902af9131e5dedd3a33bf69ebefd9340acf39
Bernardo Pacheco <berna.ipach@gmail.com>
2016-05-04 17:14:45 -0400

    Started implementation of firewall rule component

--- scripts/components/FirewallRule.jsx
+++ scripts/components/FirewallRule.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const FirewallRule = (props) => {
+const FirewallRule = ({addFirewallRule}) => {
   return (
     <div className='row'>
       <div className='col-md-12'>
         <div className='panel panel-default'>
-          <div className='panel-body'>Firewall Rule</div>
+          <div className='panel-body'>
+            <div className='form-inline'>
+              <div className='form-group'>
+                <label htmlFor='port'>Port</label>
+                <input type='text' className='form-control' id='port' placeholder='55500' />
+              </div>
+              <a onClick={addFirewallRule} type='button' className='btn btn-default'>Add Firewall Rule</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
