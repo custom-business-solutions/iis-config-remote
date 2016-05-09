db2bd2763ce3cc3eec9b65f4795ff4c436756497
Bernardo Pacheco <berna.ipach@gmail.com>
2016-05-09 13:10:02 -0400

    Basic communication between ipcRenderer and ipcMain working

--- scripts/components/ConfigContainer.jsx
+++ scripts/components/ConfigContainer.jsx
@@ -3,11 +3,34 @@ import FirewallRule from './FirewallRule'
 import UrlAcl from './UrlAcl'
 import ApplicationBinding from './ApplicationBinding'
 
+var ipcRenderer = window.require('electron').ipcRenderer
+
 export default class ConfigContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      'firewall-port': ''
+    }
+  }
+
+  componentDidMount () {
+    ipcRenderer.on('add-firewall-rule-reply', function (event, port) {
+      this.setState({
+        'firewall-port': port
+      })
+    }.bind(this))
+  }
+
+  addFirewallRule (port) {
+    // console.log(port)
+    ipcRenderer.send('add-firewall-rule', port)
+  }
+
   render () {
     return (
       <div className='container-fluid'>
-        <FirewallRule />
+        <FirewallRule addFirewallRule={this.addFirewallRule} port={this.state['firewall-port']} />
         <UrlAcl />
         <ApplicationBinding />
       </div>
--- scripts/components/FirewallRule.jsx
+++ scripts/components/FirewallRule.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
-const FirewallRule = ({addFirewallRule}) => {
-  return (
-    <div className='row'>
-      <div className='col-md-12'>
-        <div className='panel panel-default'>
-          <div className='panel-body'>
-            <div className='form-inline'>
-              <div className='form-group'>
-                <label htmlFor='port'>Port</label>
-                <input type='text' className='form-control' id='port' placeholder='55500' />
+export default class FirewallRule extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      'port': ''
+    }
+  }
+
+  handleChange (e) {
+    this.setState({
+      'port': e.target.value
+    })
+  }
+
+  handleClick (e) {
+    e.preventDefault()
+    this.props.addFirewallRule(this.state.port)
+  }
+
+  render () {
+    return (
+      <div className='row'>
+        <div className='col-md-12'>
+          <div className='panel panel-default'>
+            <div className='panel-body'>
+              <div className='form-inline'>
+                <div className='form-group'>
+                  <label htmlFor='port'>Port</label>
+                  <input type='text' className='form-control' id='port' placeholder='55500' value={this.state.port} onChange={this.handleChange} />
+                </div>
+                <a type='button' className='btn btn-default' onClick={this.handleClick}>Add Firewall Rule</a>
               </div>
-              <a onClick={addFirewallRule} type='button' className='btn btn-default'>Add Firewall Rule</a>
+              <div>
+                {this.props.port}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default FirewallRule
+FirewallRule.propTypes = {
+  addFirewallRule: PropTypes.func.isRequired,
+  port: PropTypes.string.isRequired
+}
